9c366078d17393efc6ea82c130345fd883b59946
RidoyMia <ridoymia153159hero@gmail.com>
2024-02-14 20:57:07 -0800

    seller page design completed

--- src/app/(defaultLayout)/seller/page.jsx
+++ src/app/(defaultLayout)/seller/page.jsx
@@ -1,8 +1,9 @@
 "use client"
+import axios from 'axios';
 import Link from 'next/link';
 import React from 'react';
 import { useForm } from "react-hook-form"
-import { FcGoogle } from "react-icons/fc";
+
 function page(props) {
     const { register, handleSubmit, watch, formState: { errors }, } = useForm()
     const onSubmit = (data) => {
@@ -17,15 +18,18 @@ function page(props) {
             if(res?.data?.data?.url){
                 const name = data?.name;
                 const email = data?.email;
-                
+                const district = data?.district;
+                const sub_district = data?.sub_district;
                 const password = data?.password;
                 const confirm = data?.confirm;
-                
+                const phone = data?.phone;
+                const village = data?.village;
                 const img = res?.data?.data?.url;
                 console.log(name,email,district,sub_district,password,confirm,phone,village,img)
             }
          })
        
+        
     }
     return (
         <div>
@@ -38,26 +42,30 @@ function page(props) {
                        <input type='text' placeholder='Enter your name' className='mb-6 py-2  outline-none px-5 w-full' {...register("name")} />
                        <input type='email' placeholder='Enter your email' className='mb-6 py-2  outline-none px-5 w-full' {...register("email")} />
                        </div>
-                       
+                       <div className='flex justify-center items-center align-middle gap-2'>
+                       <input type='text' placeholder='Enter your district' className='mb-6 py-2  outline-none px-5 w-full' {...register("district")} />
+                       <input type='number' placeholder='Enter your phone' className='mb-6 py-2  outline-none px-5 w-full' {...register("phone")} />
+                       </div>
+                       <div className='flex justify-center items-center align-middle gap-2'>
+                       <input type='text' placeholder='Enter your village' className='mb-6 py-2  outline-none px-5 w-full' {...register("village")} />
+                       <input type='text' placeholder='Enter your sub-dristict' className='mb-6 py-2  outline-none px-5 w-full' {...register("sub_district")} />
+                       </div>
                        <div className='flex justify-center items-center align-middle gap-2'>
                        <input type='password' placeholder='Enter your password' className='mb-6 py-2  outline-none px-5 w-full' {...register("password")} />
                        <input type='password' placeholder='Confirm' className='mb-6 py-2  outline-none px-5 w-full' {...register("confirm")} />
                        </div>
                        <input type='file' placeholder='' className='mb-6 py-10 border  outline-none px-5 w-full' {...register("image")} />
                        
+                       <div className='flex justify-center align-middle items-center gap-7'>
+                   <h1 className='text-center py-4 '><Link className=' underline text-blue-600' href="/login">Login</Link></h1> 
+                   
+                   </div>
                        <div>
-                            <button type='submit' className='bg-green-600 w-full font-semibold hover:bg-green-800 my-4 px-10 py-1 rounded-md text-white'>Register</button>
+                            <button type='submit' className='bg-green-600 w-full font-semibold hover:bg-green-800 my-4 px-10 py-1 rounded-md text-white'>Create seller</button>
                         </div>
                     </form>
-                   <div className='flex justify-center align-middle items-center gap-7'>
-                   <h1 className='text-center py-4 '><Link className=' underline text-blue-600' href="/login">Login</Link></h1> <h1>Or</h1>
-                   <h1 className='text-center py-4 '><Link className=' underline text-blue-600' href="/seller">Become Seller</Link></h1>
-                   </div>
-                    <div className="flex flex-col w-full border-opacity-50">
-                        
-                        <div className="divider">OR</div>
-                        <button className='flex mt-5 justify-center bg-yellow-400 py-1 gap-2 rounded-md align-middle items-center'><FcGoogle className='font-5xl'></FcGoogle>Google</button>
-                    </div>
+                  
+                   
 
                     
                 </div>
